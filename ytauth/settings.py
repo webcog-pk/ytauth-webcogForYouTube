7dd61d128331eaef0641e16972a7e1a2f20c9adc
Wajahat Murtaza <wajahatmurtaza144@gmail.com>
2024-07-03 12:44:57 +0500

    Update settings.py

--- ytauth/settings.py
+++ ytauth/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'test.webcog@gmail.com'
-EMAIL_HOST_PASSWORD = 'eejefioccpslvorp'
+EMAIL_HOST_USER = 'yourmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourapppassword'
 # app password
 # ALLAUTHSETTINS
 
